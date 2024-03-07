5681aff22040766441835e42e58f5a4416900f36
Mattias Matthiesen <mattmatthi@gmail.com>
2024-03-07 15:27:03 +0100

    Update README.md

--- polars_complex.py
+++ polars_complex.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 import numpy as np
 import polars as pl
 
@@ -14,54 +17,44 @@ def zero_quadrature(s: pl.Series):
 class ComplexArithmetic:
     def __init__(self, expr: pl.Expr):
         self._expr = expr
-        self._names = expr.meta.root_names()
-        self._name = expr.meta.output_name()
 
-    @staticmethod
-    def struct(real: pl.Expr | str, imag: pl.Expr | str):
-        if isinstance(real, str):
-            real = pl.col(real)
-        if isinstance(imag, str):
-            imag = pl.col(imag)
-        name = real.meta.output_name().strip(".real")
-        return pl.struct(real.alias("real"), imag.alias("imag")).alias(f"{name}[c]")
+    @property
+    def names(self):
+        meta_dict = json.loads(self._expr.meta.serialize())
+        if self._expr.meta.has_multiple_outputs():
+            names = _finditem(meta_dict, "Columns")
+            return names
+        else:
+            name = self._expr.meta.output_name()
+            name = re.sub(r"\.real|\.imag", "", name)
+            return [name]
 
     def cformat(self, name: str):
         return name if name.endswith("[c]") else name + "[c]"
 
-    def struct_repr(self, name: str = None):
-        match self._names:
-            case [str()]:
-                stem = self._names[0]
-                if name:
-                    new_stem = name.replace("[c]", "")
-                else:
-                    new_stem = stem
-                return pl.struct(
-                    pl.col(f"{stem}.real").alias("real"),
-                    pl.col(f"{stem}.imag").alias("imag"),
-                ).alias(f"{new_stem}[c]")
-            case [str(), str()]:
-                real_str = self._names[0].replace(".real", "")
-                imag_str = self._names[1].replace(".imag", "")
-                stem = real_str if (real_str == imag_str) else name
-                return pl.struct(
-                    pl.col(self._names[0]).alias("real"),
-                    pl.col(self._names[1]).alias("imag"),
-                ).alias(f"{stem}[c]")
-            case _:
+    def struct(self, name: str = None):
+        if self._expr.meta.has_multiple_outputs():
+            name_real, name_imag = self.names
+            stem = name or name_real.replace(".real", "")
+            return pl.struct(pl.col(name_real), pl.col(name_imag)).alias(
+                self.cformat(stem)
+            )
+        else:
+            stem = self.names[0]
+            if ".real" in stem:
                 raise ValueError(
-                    "Invalid expression used in `struct_repr`. Must be struct of length 2."
+                    'Provide a column `name` with ".real"/".imag" omitted. '
+                    "Corresponding `name`.real and `name`.imag columns are assumed."
                 )
-
-    def nest(self, name: str = None):
-        if name:
-            name = self.cformat(name)
-        return self.struct_repr(name)
+            alias = self.cformat(name or stem)
+            return pl.struct(
+                pl.col(f"{stem}.real").alias(alias.replace("[c]", ".real")),
+                pl.col(f"{stem}.imag").alias(alias.replace("[c]", ".imag")),
+            ).alias(alias)
 
     def unnest(self):
         real, imag = self._explode()
-        stem = self._name.replace("[c]", "")
+        stem = self.names[0].replace("[c]", "")
         real = real.alias(f"{stem}.real")
         imag = imag.alias(f"{stem}.imag")
         return real, imag
@@ -92,50 +85,90 @@ class ComplexArithmetic:
         )
         return self._expr.head(1).cast(pl.Float64) + corrected_diff
 
-    def modulus(self):
+    def modulus(self, name: str = None):
         real, imag = self._explode()
+        if name:
+            return (real.pow(2) + imag.pow(2)).sqrt().alias(name)
         return (real.pow(2) + imag.pow(2)).sqrt()
 
-    def conj(self):
+    def conj(self, name: str = None):
         real, imag = self._explode()
-        return self.struct(real, -imag).alias(self._name)
+        imag *= -1
+        if name:
+            name = self.cformat(name)
+            return pl.struct(
+                real.alias(name.replace("[c]", ".real")),
+                imag.alias(name.replace("[c]", ".imag")),
+            ).alias(name)
+        return pl.struct(real, imag).alias(self.names[0])
 
-    def quotient(self, expr):
+    def quotient(self, expr, name: str = None):
         self._expr = self._expr.name.suffix_fields(".1")
         real1, imag1 = self._explode()
         real2, imag2 = expr.name.suffix_fields(".2").complex._explode()
         real = (real1 * real2 + imag1 * imag2) / (real2.pow(2) + imag2.pow(2))
         imag = (real2 * imag1 - real1 * imag2) / (real2.pow(2) + imag2.pow(2))
-        return self.struct(real, imag).alias(self._name)
+        if name:
+            alias = self.cformat(name)
+        else:
+            alias = self.cformat(self.names[0])
+        real = real.alias(alias.replace("[c]", ".real"))
+        imag = imag.alias(alias.replace("[c]", ".imag"))
+        return pl.struct(real, imag).alias(alias)
 
-    def divide(self, expr):
-        return self.quotient(expr)
+    def divide(self, expr, name: str = None):
+        return self.quotient(expr, name=name)
 
-    def ratio(self, expr):
-        return self.quotient(expr)
+    def ratio(self, expr, name: str = None):
+        return self.quotient(expr, name=name)
 
-    def inverse(self):
+    def inverse(self, name: str = None):
         real, imag = self._explode()
-        denom = real.pow(2) + imag.pow(2)
-        return self.struct(real / denom, -imag / denom).alias(self._name)
+        real /= real.pow(2) + imag.pow(2)
+        imag /= -1 * (real.pow(2) + imag.pow(2))
+        if name:
+            alias = self.cformat(name)
+        else:
+            alias = self.cformat(self.names[0])
+        real = real.alias(alias.replace("[c]", ".real"))
+        imag = imag.alias(alias.replace("[c]", ".imag"))
+        return pl.struct(real, imag).alias(alias)
 
-    def multiply(self, expr):
+    def multiply(self, expr, name: str = None):
         a, b = self._explode()
         c, d = expr.complex._explode()
         real = a * c - b * d
         imag = a * d + b * c
-        return self.struct(real, imag).alias(self._name)
+        if name:
+            alias = self.cformat(name)
+        else:
+            alias = self.cformat(self.names[0])
+        real = real.alias(alias.replace("[c]", ".real"))
+        imag = imag.alias(alias.replace("[c]", ".imag"))
+        return pl.struct(real, imag).alias(alias)
 
-    def subtract(self, expr):
+    def subtract(self, expr, name: str = None):
         real, imag = (self._expr - expr).complex._explode()
-        return self.struct(real, imag).alias(self._name)
+        if name:
+            alias = self.cformat(name)
+        else:
+            alias = self.cformat(self.names[0])
+        real = real.alias(alias.replace("[c]", ".real"))
+        imag = imag.alias(alias.replace("[c]", ".imag"))
+        return pl.struct(real, imag).alias(alias)
 
-    def difference(self, expr):
-        return self.subtract(expr)
+    def difference(self, expr, name: str = None):
+        return self.subtract(expr, name=name)
 
-    def relative_difference(self, expr):
+    def relative_difference(self, expr, name: str = None):
         real, imag = self.quotient(expr).complex._explode()
-        return self.struct(real - 1, imag)
+        if name:
+            alias = self.cformat(name)
+        else:
+            alias = self.cformat(self.names[0])
+        real = (real - 1).alias(alias.replace("[c]", ".real"))
+        imag = imag.alias(alias.replace("[c]", ".imag"))
+        return pl.struct(real, imag).alias(alias)
 
 
 @pl.api.register_dataframe_namespace("complex")
@@ -249,3 +282,31 @@ def pl_fft(df, xname, id_vars=None):
                 )
             )
     return pl.concat(frames)
+
+
+def _finditem(d, key="Columns"):
+    """
+    Recursive function to search for the first occurrence of the key `key` in a
+    nested dictionary of mixed dict and list values.
+
+    Parameters:
+        d (dict): The dictionary to search in.
+        key (str): The key to search for, default is "Columns".
+
+    Returns:
+        list or None: The value associated with the key "Columns", or None if not found.
+    """
+    if isinstance(d, dict):
+        if key in d:
+            return d[key]
+        else:
+            for value in d.values():
+                result = _finditem(value, key)
+                if result is not None:
+                    return result
+    elif isinstance(d, list):
+        for item in d:
+            result = _finditem(item, key)
+            if result is not None:
+                return result
+    return None
